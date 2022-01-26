c74347cc3ceea3d31b5b18ca5f1616d784464ea4
black2st11 <black2st11@gmail.com>
2022-01-26 20:51:16 +0900

    2022-01-26 추가

--- implementation/dummy.py
+++ implementation/dummy.py
@@ -1,29 +1,68 @@
+"""
+https://www.acmicpc.net/problem/3190
+"""
 import sys
+from collections import deque
 
 n = int(sys.stdin.readline().rstrip())
 
-game_map = []
+maps = [[0] * n for _ in range(n)]
 
-for i in range(n):
-    if i == 0 or i == n - 1:
-        game_map.append([1] * n)
-    else:
-        game_map.append([1] + [0] * (n - 2) + [1])
 
 apple_count = int(sys.stdin.readline().rstrip())
 
+
 for _ in range(apple_count):
     x, y = map(int, sys.stdin.readline().rstrip().split())
-
-    game_map[x - 1][y - 1] = 2
+    maps[x - 1][y - 1] = 1
 
 spell_count = int(sys.stdin.readline().rstrip())
-spell = []
+spells = []
 
 for _ in range(spell_count):
-    spell.append(sys.stdin.readline().rstrip().split())
+    spells.append(sys.stdin.readline().rstrip().split())
 
 second = 0
-snake = [[0] * apple_count for _ in range(len(apple_count))]
+x = 1
+y = 1
+
+snake = deque()
+snake.append((x, y))
+
+# 0은 동, 1은 남, 2는 서, 3은 북
+
+direction = 0
+
+# x는 행, y는 열
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+maps[x - 1][y - 1] = 2
 
 while True:
+    second += 1
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+
+    if nx <= n and nx > 0 and ny <= n and ny > 0 and maps[nx - 1][ny - 1] != 2:
+        if maps[nx - 1][ny - 1] == 1:
+            maps[nx - 1][ny - 1] = 2
+        else:
+            px, py = snake.pop()
+            maps[px - 1][py - 1] = 0
+
+        snake.appendleft((nx, ny))
+        maps[nx - 1][ny - 1] = 2
+
+        x = nx
+        y = ny
+    else:
+        break
+
+    for spell in spells:
+        if int(spell[0]) == second:
+            if spell[1] == "L":
+                direction = 3 if direction - 1 < 0 else direction - 1
+            else:
+                direction = 0 if direction + 1 > 3 else direction + 1
+
+print(second)
